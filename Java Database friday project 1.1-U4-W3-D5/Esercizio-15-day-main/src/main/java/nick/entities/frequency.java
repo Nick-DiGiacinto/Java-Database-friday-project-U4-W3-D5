3aac38c6037149543cae7732fc2372220305fe73
Nick-DiGiacinto <151628711+Nick-DiGiacinto@users.noreply.github.com>
2024-04-04 12:35:56 +0200

    Deleted third not useful and ininteded folder to prevent problems in the application.

--- Java Database friday project 1.1-U4-W3-D5/Esercizio-15-day-main/src/main/java/nick/entities/frequency.java
+++
@@ -1,6 +0,0 @@
-package nick.entities;
-
-// Creo un'enum per ripartire la frequenza di uscita dei magazines in tre periodicità diverse, come richiesto dalla traccia
-public enum frequency {
-    weekly, monthly,semiannual
-}
